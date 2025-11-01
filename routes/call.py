8543a1c7e8373d4edcaf54b05ee32344804c13f6
humamchoudhary <humamchoudhary@gmail.com>
2025-11-01 21:55:01 +0500

    update

--- routes/call.py
+++ routes/call.py
@@ -109,7 +109,7 @@ def get_voice():
     stream_gemini.parameter(name='From', value=f'{request.values.get('From', None)}')
     stream_gemini.parameter(name='Caller', value=f'{request.values.get('Caller', None)}')
 
-    stream_gemini.parameter(name='name', value=f'{request.values.get('name', None)}')
+    stream_gemini.parameter(name='name', value=f'{request.values.get('name', "phone caller")}')
     stream_gemini.parameter(name='qid', value=f'{request.values.get('qid', None)}')
  
     response.append(connect_gemini)
@@ -119,7 +119,7 @@ def get_voice():
     # Connect to your WebSocket stream
     op_dial = response.dial()
     op_dial.conference(
-        f"{request.values.get('name', None)}-{request.values.get('qid', None)}",
+        f"{request.values.get('name', None)}-{request.values.get('qid', None)}-{request.values.get('From', None)}",
           startConferenceOnEnter= True,
           endConferenceOnExit= True,    
         status_callback_event="start end join leave mute hold speaker",
