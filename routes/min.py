f4d3e5927521125214bdce413924352a2aa8758f
humamchoudhary <humamchoudhary@gmail.com>
2025-11-27 01:52:50 +0500

    update

--- routes/min.py
+++ routes/min.py
@@ -96,7 +96,7 @@ def login(subject):
 
 @min_bp.route('onboarding', methods=['GET'])
 def onboard():
-    session.clear()
+    pprint(session.items())
     return render_template('user/min-onboard.html')
 
 
